c5eba10e3f4355a92087d7ade1f6ae1fba91408b
Mohammed Irfaan Kamroodeen <irfaan.kamo@gmail.com>
2025-10-01 00:15:56 +0200

    feat: implemented basic api security features

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn'],
   });
 
+  // CORS
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: false, // no cookies since anonymous
   });
+
+  // Security headers
+  app.use(helmet());
+
+  // Rate limiter
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // each IP can hit 100 requests per window
+      standardHeaders: true, // return rate limit info in headers
+      legacyHeaders: false,
+      message: 'Too many requests, please try again later.',
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3001);
 }
+
 bootstrap();
