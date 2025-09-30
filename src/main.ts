7bb7355ecb96071feeb022ee061256fbe70b428d
Mohammed Irfaan Kamroodeen <irfaan.kamo@gmail.com>
2025-09-30 12:10:32 +0200

    feat: enabled cors

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-  logger: ['error', 'warn'],
-});
-  await app.listen(process.env.PORT ?? 3000);
+    logger: ['error', 'warn'],
+  });
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+  });
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
