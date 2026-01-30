b98f12eaf295e2769d03f5e3b88fa1e34206e268
Mohammed Irfaan Kamroodeen <irfaan.kamo@gmail.com>
2026-01-30 15:40:17 +0200

    feat: added mongodb and users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { QuranApiModule } from './quran-api/quran-api.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // makes ConfigService available everywhere
-    }),QuranApiModule],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('DATABASE_URL'),
+        dbName: 'tadabbur',
+      }),
+    }),
+    QuranApiModule,
+    UsersModule,
+  ],
 })
 export class AppModule {}
---
+++ src/users/users.service.ts
@@ -0,0 +1,47 @@
+import { Injectable, ConflictException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
+import { User, UserDocument } from './schemas/user.schema';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectModel(User.name)
+    private readonly userModel: Model<UserDocument>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const { name, email, password } = createUserDto;
+
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    try {
+      const createdUser = await this.userModel.create({
+        name,
+        email,
+        passwordHash,
+      });
+
+      const userObject: any = createdUser.toObject();
+      delete userObject.passwordHash;
+      
+      return userObject;
+    } catch (err: any) {
+      if (err.code === 11000 && err.keyPattern?.email) {
+        throw new ConflictException('Email already exists');
+      }
+      throw err;
+    }
+  }
+
+  async findAll() {
+    const users = await this.userModel.find().select('-passwordHash').exec();
+    return users;
+  }
+
+  async findByEmail(email: string) {
+    return this.userModel.findOne({ email }).exec();
+  }
+}
